21cb39c948c08cf83352047d45fcef67508f0970
Francisco Sousa <franciscoeds1987@gmail.com>
2025-02-15 22:01:22 -0300

    Control for multiple simultaneous transactions

--- src/main/java/br/com/vr/mini_autorizador/domain/repository/CartaoRepository.java
+++ src/main/java/br/com/vr/mini_autorizador/domain/repository/CartaoRepository.java
@@ -1,12 +1,15 @@
 package br.com.vr.mini_autorizador.domain.repository;
 
 import br.com.vr.mini_autorizador.domain.model.CartaoEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface CartaoRepository extends JpaRepository<CartaoEntity, String> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<CartaoEntity> findByNumeroCartao(String cardNumber);
 
 }
